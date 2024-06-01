0a1f81bf757af6dacc515fc989276d7b8602aa94
Sumit123sm <141501113+Sumit123sm@users.noreply.github.com>
2024-06-01 12:43:44 +0530

    day ass 2

---
+++ day 1/ass1/input_operator/q4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r, h;
+    cin >> r >>h;
+    cout<<" volume of cylinder is "<<3.14*r*2*h<<endl;
+    
+}
---
+++ day 1/ass1/input_operator/q5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a='a';
+    char b='b';
+    cout<<"difference between ASCII of two characters "<<(a-b)<<endl;
+
+}
